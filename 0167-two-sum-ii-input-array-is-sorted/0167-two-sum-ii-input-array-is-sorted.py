d90cc21f5102a1eae7c8edca63bd4f339cfac44d
Rohit Sharma <38152752+axalion@users.noreply.github.com>
2024-12-02 14:17:16 -0500

    Time: 7 ms (28.84%), Space: 18.1 MB (6.26%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,9 +5,9 @@ class Solution:
         while l <= r:
             if numbers[l]+numbers[r] == target:
                 return [l+1, r+1]
-            elif numbers[l] + numbers[r] > target:
+            if numbers[l] + numbers[r] > target:
                 r-=1
-            elif numbers[l] + numbers[r] < target:
+            else:
                 l+=1
         
              
